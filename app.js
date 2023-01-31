3f7e8616e6dbd092ba415358aa4c3f0d6e66b8e2
Sumit Singh Rajput <itssumit258@gmail.com>
2023-01-31 18:56:55 +0530

    added modal

--- app.js
+++ app.js
@@ -10,37 +10,41 @@ let computerChoice;
 let result;
 
 possibleChoices.forEach((possibleChoice) =>
-  possibleChoice.addEventListener("click", (e) => {
-    userChoice = e.target.id;
-    // userChoiceDisplay.innerText = userChoice;
-    if (userChoice === "rock") {
-      // userChoiceDisplay.innerHTML =
-      //   "<img class='choice' src='assets/rock.jpg' alt='rock'>";
-      generateComputerChoice();
-      getResult();
-      rock.classList.add("animate-up");
-      paper.classList.remove("animate-up");
-      scicssors.classList.remove("animate-up");
-    }
-    if (userChoice === "paper") {
-      // userChoiceDisplay.innerHTML =
-      //   "<img class='choice' src='assets/paper.jpg' alt='rock'>";
-      generateComputerChoice();
-      getResult();
-      paper.classList.add("animate-up");
-      rock.classList.remove("animate-up");
-      scicssors.classList.remove("animate-up");
-    }
-    if (userChoice === "scissors") {
-      // userChoiceDisplay.innerHTML =
-      //   "<img class='choice' src='assets/scissors.jpg' alt='rock'>";
-      generateComputerChoice();
-      getResult();
-      scicssors.classList.add("animate-up");
-      rock.classList.remove("animate-up");
-      paper.classList.remove("animate-up");
-    }
-  })
+  possibleChoice.addEventListener(
+    "click",
+    (e) => {
+      userChoice = e.target.id;
+      // userChoiceDisplay.innerText = userChoice;
+      if (userChoice === "rock") {
+        // userChoiceDisplay.innerHTML =
+        //   "<img class='choice' src='assets/rock.jpg' alt='rock'>";
+        generateComputerChoice();
+        getResult();
+        rock.classList.add("animate-up");
+        paper.classList.remove("animate-up");
+        scicssors.classList.remove("animate-up");
+      }
+      if (userChoice === "paper") {
+        // userChoiceDisplay.innerHTML =
+        //   "<img class='choice' src='assets/paper.jpg' alt='rock'>";
+        generateComputerChoice();
+        getResult();
+        paper.classList.add("animate-up");
+        rock.classList.remove("animate-up");
+        scicssors.classList.remove("animate-up");
+      }
+      if (userChoice === "scissors") {
+        // userChoiceDisplay.innerHTML =
+        //   "<img class='choice' src='assets/scissors.jpg' alt='rock'>";
+        generateComputerChoice();
+        getResult();
+        scicssors.classList.add("animate-up");
+        rock.classList.remove("animate-up");
+        paper.classList.remove("animate-up");
+      }
+    },
+    true
+  )
 );
 
 function generateComputerChoice() {
@@ -87,3 +91,28 @@ function getResult() {
 
   resultDisplay.innerText = result;
 }
+
+var modal = document.getElementById("myModal");
+
+// Get the button that opens the modal
+var btn = document.getElementById("myBtn");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks the button, open the modal
+btn.onclick = function () {
+  modal.style.display = "block";
+};
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function () {
+  modal.style.display = "none";
+};
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function (event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+};
